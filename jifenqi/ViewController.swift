8e14ff0af847200d5504dac4cd545b56153bfe75
wangyufei993 <1531481086@qq.com>
2016-05-05 10:51:18 +0800

    jiemian
    jiemian

--- jifenqi/ViewController.swift
+++ jifenqi/ViewController.swift
@@ -15,11 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func add1(sender: AnyObject) {
+        lable1.text=String(Int(lable1.text!)!+1)
+    }
 
+    @IBAction func add2(sender: AnyObject) {
+        lable2.text=String(Int(lable2.text!)!+1)
+    }
 
+    @IBOutlet weak var lable1: UILabel!
+    @IBOutlet weak var lable2: UILabel!
 }
 
