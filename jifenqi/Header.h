6a6c2f6ed180bc73f03a1e5a1d9542ffdfceb911
wangyufei993 <1531481086@qq.com>
2016-05-05 11:55:03 +0800

    shujuku
    shujuku

---
+++ jifenqi/Header.h
@@ -0,0 +1,10 @@
+//
+//  Header.h
+//  jifenqi
+//
+//  Created by 20131105755 on 16/5/5.
+//  Copyright © 2016年 20131105755. All rights reserved.
+//
+
+#import "sqlite3.h"
+#import <time.h>
--- jifenqi/ViewController.swift
+++ jifenqi/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var db :SQLiteDB!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        //如果有数据则加载
+        initUser()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    //从SQLite加载数据
+    @IBAction func saveClicked(sender: AnyObject) {
+        saveUser()
+    }
+    func initUser() {
+        let data = db.query("select * from t_user")
+        if data.count > 0 {
+            //获取最后一行数据显示
+            let user = data[data.count - 1]
+            lable1.text = user["uname"] as? String
+            lable2.text = user["mobile"] as? String
+        }
+    }
+    
+    //保存数据到SQLite
+    func saveUser() {
+        let uname = self.lable1.text!
+        let mobile = self.lable2.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
     }
 
     
